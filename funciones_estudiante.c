ad46a629825276afaf0263003350ff6933ff0942
CENTRAL\185429 <185429@coto.com.ar>
2024-04-30 23:17:43 -0300

    Se realizan los argumentos a main

--- funciones_estudiante.c
+++ funciones_estudiante.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
 #include "estructuras.h"
 #include "funciones_estudiante.h"
 #include "constantes.h"
@@ -50,204 +52,288 @@
  *	DATA:	X	Pixels
  */
 
-void solucion(int argc, char* argv[]){
+void solucion(int argc, char* argv[])
+{
     // agregar lectura de argumentos.
     t_pixel img;
     t_metadata header;
 
-
-
     //declarar y que la funcion retorne header e imagen
-    openBmpFile(&img, &header);
+    openBmpFile(argv, &img, &header);
 
 
 
-    //free(img);
+
     /*
         Aquí deben hacer el código que solucione lo solicitado.
         Todas las funciones utilizadas deben estar declaradas en este archivo, y en su respectivo .h
     */
 }
 
-void openBmpFile(t_pixel *img, t_metadata *header){
-    FILE *pf;
-    //abro el archivo
-    pf = fopen("unlam.bmp", "rb");
+void openBmpFile(char* argv[], t_pixel *img, t_metadata *header)
+{
+    int i = 1;
+    bool contieneImagen = false;
+    int contImagenes = 0;
+    while(argv[i])
+    {
+        if(strstr(argv[i], "--") == 0)
+        {
+            contieneImagen = true;
+            contImagenes++;
+        }
+        i++;
+    }
 
-    if (pf == NULL) {
-        printf("No se pudo abrir el archivo.");
+    if(contImagenes > 1)
+    {
+        printf("\nSe debe pasar solo una imagen como argumento.\n");
         return;
     }
 
-    if(getc(pf) == 'B' && getc(pf) == 'M'){ // aca estoy parado en el byte 2
-        fread(&header->tamArchivo, sizeof(unsigned int), 1, pf);
-        fseek(pf, 4, SEEK_CUR); // me muevo 4 bytes, para posicionarme en el byte 10
-                                // este byte nro 10 contiene el inicio de la imagen
-        fread(&header->comienzoImagen, sizeof(unsigned int), 1, pf);// hago el fread, como es unsigned int ocupa 4bytes,
-                                                                    // y el puntero queda en el byte 14, donde se encuentra el tamanio del header
-        fread(&header->tamEncabezado, sizeof(unsigned int), 1, pf);
-        fread(&header->ancho, sizeof(unsigned int), 1, pf);
-        fread(&header->alto, sizeof(unsigned int), 1, pf);
-        fseek(pf, 2, SEEK_CUR);
-        fread(&header->profundidad, sizeof(unsigned short), 1, pf);
+    i = 1;
+    if(contieneImagen)
+    {
+        while(argv[i])
+        {
+            if(strstr(argv[i], "--") == 0)
+            {
+                //abro el archivo
+                FILE *pf;
+                strcat(argv[i], ".bmp");
+                pf = fopen(argv[i], "rb");
+                if (pf == NULL)
+                {
+                    printf("No se pudo abrir el archivo.");
+                    return;
+                }
 
-        printf("\n tam arch: %d \n comienzo: %d \n tam enca: %d \n ancho: %d \n alto: %d \n prof %d ",
-               header->tamArchivo, header->comienzoImagen, header->tamEncabezado, header->ancho,
-               header->alto, header->profundidad
-               );
+                if(getc(pf) == 'B' && getc(pf) == 'M')
+                {
+                    fread(&header->tamArchivo, sizeof(unsigned int), 1, pf);
+                    fseek(pf, 4, SEEK_CUR);
+                    fread(&header->comienzoImagen, sizeof(unsigned int), 1, pf);
 
-        // leo la imagen
-        img = (t_pixel *)malloc(header->alto * header->ancho * sizeof(t_pixel));
-        t_pixel *ini = img;
-        if(img == NULL){
-            printf("\n No se pudo reservar memoria para la imagen.");
-            return;
-        }
+                    fread(&header->tamEncabezado, sizeof(unsigned int), 1, pf);
+                    fread(&header->ancho, sizeof(unsigned int), 1, pf);
+                    fread(&header->alto, sizeof(unsigned int), 1, pf);
+                    fseek(pf, 2, SEEK_CUR);
+                    fread(&header->profundidad, sizeof(unsigned short), 1, pf);
 
-        fseek(pf, header->comienzoImagen, SEEK_SET);
-        //fread(img->pixel, sizeof(img->pixel),header->alto * header->ancho,pf);
-        for( int i=0; i< header->alto; i++){
-            for(int j=0; j<header->ancho; j++){
-                fread(img[i*header->ancho+j].pixel, sizeof(unsigned char), 3, pf);
+                    printf("\n tam arch: %d \n comienzo: %d \n tam enca: %d \n ancho: %d \n alto: %d \n prof %d ",
+                           header->tamArchivo, header->comienzoImagen, header->tamEncabezado, header->ancho,
+                           header->alto, header->profundidad
+                          );
+
+                    img = (t_pixel *)malloc(header->alto * header->ancho * sizeof(t_pixel));
+                    t_pixel *ini = img;
+                    if(img == NULL)
+                    {
+                        printf("\n No se pudo reservar memoria para la imagen.");
+                        return;
+                    }
+
+                    fseek(pf, header->comienzoImagen, SEEK_SET);
+                    for( int i=0; i< header->alto; i++)
+                    {
+                        for(int j=0; j<header->ancho; j++)
+                        {
+                            fread(img[i*header->ancho+j].pixel, sizeof(unsigned char), 3, pf);
+                        }
+                    }
+
+                    printf("Primer pixel: R=%u, G=%u, B=%u\n", img[0].pixel[0], img[0].pixel[1], img[0].pixel[2]);
+
+                    definirEfecto(argv, header, img);
+                    img = ini;
+                    free(img);
+                    fclose(pf);
+                }
+                else
+                {
+                    printf("\n El archivo indicado no es de tipo bit map.");
+                }
             }
+
+            i++;
         }
-
-//        unsigned int padding = (4 - (header->ancho * sizeof(img->pixel)) % 4) % 4; // Calcula el padding
-//        for (int i = 0; i < header->alto; i++) {
-//            fread(img + i * header->ancho, sizeof(img->pixel),header->ancho, pf);
-//            fseek(pf, padding, SEEK_CUR); // Salta los bytes de relleno
-//        }
-
-          printf("Primer pixel: R=%u, G=%u, B=%u\n", img[0].pixel[0], img[0].pixel[1], img[0].pixel[2]);
-
-
-
-
-//        // mostrar imagen
-
-
-        // archivo de salida
-        //crearBmpSalida(img, header);
-        //escalaDeGrises(img, header->alto, header->ancho);
-        crearBmpSalida(img, header, "estudianteV2.bmp");
-        img = ini;
-        //escalaDeGrises(img, header);
-        //imgNegativa(img, header);
-        //aumentar25Contraste(img, header);
-        reducir25Contraste(img,header);
-        fclose(pf);
-
-    }else{
-        printf("\n El archivo indicado no es de tipo bit map.");
     }
-
-
-
-    fclose(pf);
-
+    else
+    {
+        printf("No recibe imagen");
+        return;
+    }
 }
 
 // funcion que convierte la imagen a escala de grises
-int escalaDeGrises(t_pixel *imagen, t_metadata *header) {
-    for (int i = 0; i < header->alto * header->ancho; i++) {
-        unsigned char promedio = (imagen[i].pixel[0] + imagen[i].pixel[1] + imagen[i].pixel[2]) / 3;
-        for (int j = 0; j < 8; j++) {
-            imagen[i].pixel[j] = promedio;
+int escalaDeGrises(t_pixel *imagen, t_metadata *header, char nombre[])
+{
+    t_pixel * imgCopy = malloc(sizeof(t_pixel)*header->alto*header->ancho);
+    memcpy(imgCopy, imagen, sizeof(t_pixel)*header->alto*header->ancho);
+
+    for (int i = 0; i < header->alto * header->ancho; i++)
+    {
+        unsigned char promedio = (imgCopy[i].pixel[0] + imgCopy[i].pixel[1] + imgCopy[i].pixel[2]) / 3;
+        for (int j = 0; j < 8; j++)
+        {
+            imgCopy[i].pixel[j] = promedio;
         }
     }
 
-    crearBmpSalida(imagen, header, "escala_grises.bmp");
+    crearBmpSalida(imgCopy, header, nombre);
+    free(imgCopy);
 
     return 0;
 }
 
-int imgNegativa(t_pixel *imagen, t_metadata * header){
+int imgNegativa(t_pixel *imagen, t_metadata * header, char nombre[])
+{
+    t_pixel * imgCopy = malloc(sizeof(t_pixel)*header->alto*header->ancho);
+    memcpy(imgCopy, imagen, sizeof(t_pixel)*header->alto*header->ancho);
 
-     for (int i = 0; i < header->alto * header->ancho; i++) {
-        for (int j = 0; j < 8; j++) {
-            imagen[i].pixel[j] = 255 - imagen[i].pixel[j];
+    for (int i = 0; i < header->alto * header->ancho; i++)
+    {
+        for (int j = 0; j < 8; j++)
+        {
+            imgCopy[i].pixel[j] = 255 - imgCopy[i].pixel[j];
         }
     }
 
-    crearBmpSalida(imagen, header, "negativo.bmp");
+    crearBmpSalida(imgCopy, header, nombre);
+    free(imgCopy);
     return 0;
 }
 
-int aumentar25Contraste(t_pixel *imagen, t_metadata *header){
+int aumentar25Contraste(t_pixel *imagen, t_metadata *header, char nombre[])
+{
     float porcentaje = 1.25;
     float valor_original = 0;
     float valor_nuevo = 0;
 
-    for (int i = 0; i < header->alto * header->ancho; i++) {
-        for (int j = 0; j < 3; j++) {
-            valor_original = imagen[i].pixel[j];
+    t_pixel * imgCopy = malloc(sizeof(t_pixel)*header->alto*header->ancho);
+    memcpy(imgCopy, imagen, sizeof(t_pixel)*header->alto*header->ancho);
+
+    for (int i = 0; i < header->alto * header->ancho; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            valor_original = imgCopy[i].pixel[j];
             valor_nuevo = valor_original * porcentaje;
-            if(valor_nuevo < 0){
+            if(valor_nuevo < 0)
+            {
                 valor_nuevo = 0;
-            }else if(valor_nuevo > 255){
+            }
+            else if(valor_nuevo > 255)
+            {
                 valor_nuevo = 255;
             }
-            imagen[i].pixel[j] = (unsigned char)valor_nuevo;
+            imgCopy[i].pixel[j] = (unsigned char)valor_nuevo;
         }
     }
-    crearBmpSalida(imagen, header, "contraste25aum.bmp");
+    crearBmpSalida(imgCopy, header, nombre);
+    free(imgCopy);
     return 0;
 }
 
-int reducir25Contraste(t_pixel *imagen, t_metadata *header){
+int reducir25Contraste(t_pixel *imagen, t_metadata *header, char nombre[])
+{
+    t_pixel * imgCopy = malloc(sizeof(t_pixel)*header->alto*header->ancho);
+    memcpy(imgCopy, imagen, sizeof(t_pixel)*header->alto*header->ancho);
+
     float porcentaje = 0.25;
     float valor_original = 0;
     float valor_nuevo = 0;
 
-    for (int i = 0; i < header->alto * header->ancho; i++) {
-        for (int j = 0; j < 3; j++) {
-            valor_original = imagen[i].pixel[j];
+    for (int i = 0; i < header->alto * header->ancho; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            valor_original = imgCopy[i].pixel[j];
             valor_nuevo = valor_original * porcentaje;
             valor_nuevo = valor_original - valor_nuevo;
-            if(valor_nuevo < 0){
+            if(valor_nuevo < 0)
+            {
                 valor_nuevo = 0;
-            }else if(valor_nuevo > 255){
+            }
+            else if(valor_nuevo > 255)
+            {
                 valor_nuevo = 255;
             }
-            imagen[i].pixel[j] = (unsigned char)valor_nuevo;
+            imgCopy[i].pixel[j] = (unsigned char)valor_nuevo;
         }
     }
-    crearBmpSalida(imagen, header, "contraste25redu.bmp");
+
+    crearBmpSalida(imgCopy, header, nombre);
+    free(imgCopy);
     return 0;
 }
 
 // funcion que cree un archivo de salida
-int crearBmpSalida(t_pixel *imagen, t_metadata *header, char nombre[20]){
+int crearBmpSalida(t_pixel *imagen, t_metadata *header, char nombre[])
+{
     FILE *pf;
     pf = fopen(nombre, "wb");
 
-    if (pf == NULL) {
+    if(!nombre)
+    {
+        printf("Error al leer el argumento");
+        return 1;
+    }
+
+    if (pf == NULL)
+    {
         printf("No se pudo abrir el archivo.");
         return 1;
     }
 
     fwrite("BM", sizeof(char), 2, pf);
     fwrite(&header->tamArchivo, sizeof(unsigned int), 1, pf);
-    fseek(pf, 4, SEEK_CUR); // me muevo 4 bytes, para posicionarme en el byte 10
-                            // este byte nro 10 contiene el inicio de la imagen
-    fwrite(&header->comienzoImagen, sizeof(unsigned int), 1, pf);// hago el fread, como es unsigned int ocupa 4bytes,
-                                                                // y el puntero queda en el byte 14, donde se encuentra el tamanio del header
+    fseek(pf, 4, SEEK_CUR);
+    fwrite(&header->comienzoImagen, sizeof(unsigned int), 1, pf);
     fwrite(&header->tamEncabezado, sizeof(unsigned int), 1, pf);
     fwrite(&header->ancho, sizeof(unsigned int), 1, pf);
     fwrite(&header->alto, sizeof(unsigned int), 1, pf);
     fseek(pf, 2, SEEK_CUR);
     fwrite(&header->profundidad, sizeof(unsigned short), 1, pf);
     fseek(pf, header->comienzoImagen, SEEK_SET);
-    for( int i=0; i< header->alto; i++){
-            for(int j=0; j<header->ancho; j++){
-                fwrite(imagen[i*header->ancho+j].pixel, sizeof(unsigned char), 3, pf);
-            }
+    for( int i=0; i< header->alto; i++)
+    {
+        for(int j=0; j<header->ancho; j++)
+        {
+            fwrite(imagen[i*header->ancho+j].pixel, sizeof(unsigned char), 3, pf);
         }
-
-    //fwrite(imagen, sizeof(imagen), header->alto * header->ancho, pf);
-
+    }
     fclose(pf);
 
     return TODO_OK;
+}
+
+void definirEfecto(char* argv[], t_metadata* header, t_pixel* img)
+{
+    int i = 1;
+    while(argv[i])
+    {
+        if (strcmp(argv[i], "--negativo") == 0)
+        {
+            imgNegativa(img, header, "negativo.bmp");
+        }
+        else if (strcmp(argv[i], "--escala-de-grises") == 0)
+        {
+            escalaDeGrises(img, header, "escala-de-grises.bmp");
+        }
+        else if (strcmp(argv[i], "--aumentar-contraste") == 0)
+        {
+            aumentar25Contraste(img, header, "aumentar-contraste.bmp");
+        }
+        else if (strcmp(argv[i], "--reducir-contraste") == 0)
+        {
+            reducir25Contraste(img, header, "reducir-contraste.bmp");
+        }
+        else if(strstr(argv[i], "--") != 0)
+        {
+            printf("\nArgumento no reconocido: %s\n\n", argv[i]);
+        }
+        i++;
+    }
 }
 
--- funciones_estudiante.h
+++ funciones_estudiante.h
@@ -3,10 +3,11 @@
 #include "estructuras.h"
 
 void solucion(int argc, char* argv[]);
-void openBmpFile(t_pixel *img, t_metadata *header);
-int escalaDeGrises(  t_pixel *imagen, t_metadata *header);
-int imgNegativa(t_pixel *imagen, t_metadata *header);
-int crearBmpSalida(t_pixel *imagen, t_metadata *header,char nombre[20]);
-int aumentar25Contraste(t_pixel *imagen, t_metadata *header);
-int reducir25Contraste(t_pixel *imagen, t_metadata *header);
+void openBmpFile(char* argv[], t_pixel *img, t_metadata *header);
+int escalaDeGrises(  t_pixel *imagen, t_metadata *header, char nombre[]);
+int imgNegativa(t_pixel *imagen, t_metadata *header, char nombre[]);
+int crearBmpSalida(t_pixel *imagen, t_metadata *header,char nombre[]);
+int aumentar25Contraste(t_pixel *imagen, t_metadata *header, char nombre[]);
+int reducir25Contraste(t_pixel *imagen, t_metadata *header, char nombre[]);
+void definirEfecto(char* argv[],t_metadata* header,t_pixel* img);
 #endif // FUNCIONES_ESTUDIANTE_H_INCLUDED
